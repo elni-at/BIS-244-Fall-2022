748ccc7264e832ad8d5cd57549fb2dba098323d0
Elnaz Torkamani <ea482@scarletmail.rutgers.edu>
2022-08-30 14:29:58 -0400

    Healy Chapter 2 files

---
+++ BIS244_CH2_packages.R
@@ -0,0 +1,18 @@
+## Load Libraries
+# To make sure you are ready to go, you need to install the following packages:
+
+my_packages <- c("tidyverse", "broom", "coefplot", "cowplot", "drat",
+                 "gapminder", "GGally", "ggrepel", "ggridges", "gridExtra",
+                 "here", "interplot", "margins", "maps", "mapproj",
+                 "mapdata", "MASS", "quantreg", "rlang", "scales", "socviz",
+                 "survey", "srvyr", "viridis", "viridisLite", "devtools",
+                 "rtools","backports","vctrs")
+
+
+install.packages(my_packages, repos = "http://cran.rstudio.com")
+
+# Install packages not yet installed
+installed_packages <- my_packages %in% rownames(installed.packages())
+if (any(installed_packages == FALSE)) {
+  install.packages(my_packages[!installed_packages])
+}
